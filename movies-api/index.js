cc0eeeca06e8e9aa29095f84cd8d6ff9341bd476
Mia Gough <miagough@Mias-MacBook-Pro.local>
2023-04-15 17:22:11 +0100

    added routing for movies and movie reviews

--- movies-api/index.js
+++ movies-api/index.js
@@ -8,7 +8,11 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.use('/api/movies', moviesRouter);
+
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
